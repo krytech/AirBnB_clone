dd37dc05989f4bb93157b8765213261b40e25802
Carson Stearn <cstearn@gmail.com>
2021-06-27 16:33:06 +0000

    adding classes

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""City class that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """A City class that inherits from BaseModel"""
+    state_id = ""
+    name = ""
